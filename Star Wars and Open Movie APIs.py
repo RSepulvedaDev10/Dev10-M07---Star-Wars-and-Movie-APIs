fccdbc8a2f96c5a2625d738b1b2143a5d1489dd1
RSepulvedaDev10 <94391613+RSepulvedaDev10@users.noreply.github.com>
2022-01-07 02:28:43 -0600

    Update Star Wars and Open Movie APIs.py

--- Star Wars and Open Movie APIs.py
+++ Star Wars and Open Movie APIs.py
@@ -1,6 +1,8 @@
-from dataclasses import dataclass, fields
+from dataclasses import dataclass, field
 import datetime as dt
 import requests
+import json
+import csv
 
 @dataclass
 class StarWarsFilms:
@@ -11,13 +13,22 @@ class StarWarsFilms:
     producer: str
     release_date: dt.date
     characters: list
+    plot: str
+    rotten_tomatoes: int
+    box_office_gross: int = field(metadata={"units":"U.S dollars"})
+    
+    def __gt__(self, other):
+        return self.episode_id > other.episode_id
+    
+    def __ge__(self, other):
+        return self.episode_id >= other.episode_id
 
 
 @dataclass
 class StarWarsCharacter:
     name: str
-    height: int
-    mass: int
+    height: int = field(metadata={"units":"cenitmeters"})
+    mass: int = field(metadata={"units":"kilograms"})
     hair_color: str
     skin_color: str
     eye_color: str
@@ -30,3 +41,5 @@ class StarWarsLibrary(StarWarsFilms):
     movies: list
     shows: list
     last_updated: dt.date
+    
+    
